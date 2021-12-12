17048182a356083c82efaec905665e0ecfc359eb
slow <slowdev14@gmail.com>
2021-12-12 12:27:20 +0100

    ☄️ Changed status system
    Set the status before presence can be the cause of bugs so I changed the status system

--- presences/game.js
+++ presences/game.js
@@ -1,9 +1,12 @@
 const chalk = require('chalk');
 const rpc = require('discordrpcgenerator');
+
 const config = require('.././config.json');
+const uuid = ()=>([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, a=>(a ^ Math.random() * 16 >> a / 4).toString(16));
 
 if (config.mode === 'game') {
 
+// Create the RPC
 const client = require('../index');
 client.on('ready', () => {
 
@@ -29,8 +32,21 @@ client.on('ready', () => {
         .setStartTimestamp(config.settings.game.startTimestamp ? config.settings.game.startTimestamp : undefined)
         .setEndTimestamp(config.settings.game.endTimestamp ? config.settings.game.endTimestamp : undefined);
 
-    client.user.setPresence(presence.toDiscord());
-    console.log(chalk.hex('#800080')('Discord RPC enabled successfully!'));
+        // Set the presence
+        client.user.setPresence(presence.toDiscord());
+
+        // Set the status
+        if (config.status === 'online' || config.status === 'idle' || config.status === 'dnd') {
+        client.user.setStatus(config.status);}
+
+        if (config.status === 'offline' || config.status === 'invisible') {
+          console.log('Status cant be set to' + config.status + '\nPlease change the status in the config.json file');}
+
+        // Done !
+        console.log(chalk.hex('#800080')('Spotify RPC enabled successfully!'));
+        console.log(chalk.hex('#800080')('Spotify: ' + config.settings.spotify.details));
+        console.log(chalk.hex('#800080')('Status: ' + config.status ? config.status : 'status not defined'));
+
 });
 
 } catch (err) {
--- presences/spotify.js
+++ presences/spotify.js
@@ -1,9 +1,11 @@
 const chalk = require('chalk');
 const rpc = require('discordrpcgenerator');
+
 const config = require('.././config.json');
 
 if (config.mode === 'spotify') {
 
+// Create the RPC
 const client = require('../index');
 client.on('ready', () => {
 
@@ -22,8 +24,20 @@ client.on('ready', () => {
     .setStartTimestamp(config.settings.spotify.startTimestamp ? config.settings.spotify.startTimestamp : undefined)
     .setEndTimestamp(config.settings.spotify.endTimestamp ? config.settings.spotify.endTimestamp : undefined);
 
+    // Set the presence
     client.user.setPresence(presence.toDiscord());
+
+    // Set the status
+    if (config.status === 'online' || config.status === 'idle' || config.status === 'dnd') {
+    client.user.setStatus(config.status);}
+
+    if (config.status === 'offline' || config.status === 'invisible') {
+      console.log('Status cant be set to' + config.status + '\nPlease change the status in the config.json file');}
+
+    // Done!
     console.log(chalk.hex('#800080')('Spotify RPC enabled successfully!'));
+    console.log(chalk.hex('#800080')('Spotify: ' + config.settings.spotify.details));
+    console.log(chalk.hex('#800080')('Status: ' + config.status ? config.status : 'status not defined'));
 
 
 } catch (err) {
