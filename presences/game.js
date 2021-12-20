7ba638bd05a2ce5b58253412e82f72f61717391e
slow <slowdev14@gmail.com>
2021-12-20 18:37:58 +0100

    fixed bugs

--- presences/game.js
+++ presences/game.js
@@ -1,52 +1,35 @@
-const config = require('../config.json');
 const client = require('..');
-
-const rpc = require('discordrpcgenerator');
 const chalk = require('chalk');
+const rpc = require('discordrpcgenerator');
+const config = require('.././config.json');
 
 if (config.mode === 'game') {
-  client.on('ready', () => {
-
-    try {
-      rpc.getRpcImage(config.settings.game.applicationID, config.settings.game.largeImageKey)
-        .then(image => {
-          const presence = new rpc.Rpc()
-            .setApplicationId(config.settings.game.applicationID)
-            .setType('PLAYING')
-            .setName(config.settings.game.name)
-
-            .setDetails(config.settings.game.details)
-            .setState(config.settings.game.state)
-
-            .setAssetsLargeImage(image.id)
-            .setAssetsLargeText(config.settings.game.largeImageText ? config.settings.game.largeText : image.name)
-
-            .setAssetsSmallImage(config.settings.game.smallImage ? config.settings.game.smallImage : undefined)
-            .setAssetsSmallText(config.settings.game.smallImageText ? config.settings.game.smallImageText : undefined)
-
-            .setStartTimestamp(config.settings.game.startTimestamp)
-            .setEndTimestamp(config.settings.game.endTimestamp);
-
-          // Set the presence
-          client.user.setPresence(presence.toDiscord());
-        });
-
-      // Set the status
-      if (config.status === 'online' || config.status === 'idle' || config.status === 'dnd') {
-        client.user.setStatus(config.status);
-      }
-
-      if (config.status === 'offline' || config.status === 'invisible') {
-        console.log('Status cant be set to' + config.status + '\nPlease change the status in the config.json file');
-      }
-
-      // Done !
-      console.log(chalk.hex('#800080')('Game RPC enabled successfully!'));
-      console.log(chalk.hex('#800080')('Game: ' + config.settings.game.name));
-      console.log(chalk.hex('#800080')('Status: ' + config.status));
-
-    } catch (err) {
-      console.log(err);
+  try {
+    rpc.getRpcImage(config.settings.game.applicationID, config.settings.largeImageKey).then(image => {
+      const presence = new rpc.Rpc()
+        .setName(config.settings.game.name)
+        .setType('PLAYING')
+        .setApplicationId(config.settings.game.applicationID)
+        .setState(config.settings.game.state)
+        .setDetails(config.settings.game.details)
+        .setAssetsLargeImage(image.id)
+        .setAssetsLargeText(image.name);
+      client.user.setPresence(presence.toDiscord());
+    });
+    // Set the status
+    if (config.status === 'online' || config.status === 'idle' || config.status === 'dnd') {
+      client.user.setStatus(config.status);
     }
-  });
+
+    if (config.status === 'offline' || config.status === 'invisible') {
+      console.log('Status cant be set to' + config.status + '\nPlease change the status in the config.json file');
+    }
+
+    // Done !
+    console.log(chalk.hex('#800080')('Game RPC enabled successfully!'));
+    console.log(chalk.hex('#800080')('Game: ' + config.settings.game.name));
+    console.log(chalk.hex('#800080')('Status: ' + config.status));
+  } catch (err) {
+    console.error(err);
+  }
 }
--- presences/twitch.js
+++ presences/twitch.js
@@ -1,52 +1,28 @@
-const config = require('../config.json');
 const client = require('..');
-
-const rpc = require('discordrpcgenerator');
 const chalk = require('chalk');
+const rpc = require('discordrpcgenerator');
+const config = require('.././config.json');
 
 if (config.mode === 'twitch') {
-  client.on('ready', () => {
+  try {
+    rpc.getRpcImage(config.settings.twitch.applicationID, config.settings.largeImageKey).then(image => {
+      const presence = new rpc.Rpc()
+        .setName(config.settings.twitch.name)
+        .setType('STREAMING')
+        .setUrl(config.settings.twitch.url)
+        .setApplicationId(config.settings.twitch.applicationID)
+        .setState(config.settings.twitch.state)
+        .setDetails(config.settings.twitch.details)
+        .setAssetsLargeImage(image.settings.twitch.id)
+        .setAssetsLargeText(image.settings.twitch.name);
+      client.user.setPresence(presence.toDiscord());
+    });
 
-    try {
-      rpc.getRpcImage(config.settings.twitch.applicationID, config.settings.twitch.largeImageKey)
-        .then(image => {
-          const presence = new rpc.Rpc()
-            .setName(config.settings.twitch.name)
-            .setUrl(config.settings.twitch.url)
-            .setDetails(config.settings.twitch.name)
-            .setType('STREAMING')
-            .setState(config.settings.twitch.details)
-            .setApplicationId(config.settings.twitch.applicationID)
+    // Done !
+    console.log(chalk.hex('#800080')('Twitch RPC enabled successfully!'));
+    console.log(chalk.hex('#800080')('Twitch: ' + config.settings.twitch.name));
 
-            .setAssetsLargeImage(config.settings.twitch.largeImageKey ? config.settings.twitch.largeImageKey : image.id)
-            .setAssetsLargeText(config.settings.twitch.largeImageText)
-
-            .setAssetsSmallImage(config.settings.twitch.smallImagekey ? config.settings.twitch.smallImageKey : undefined)
-            .setAssetsSmallText(config.settings.twitch.smallImageText ? config.settings.twitch.smallImageText : undefined)
-
-            .setStartTimestamp(config.settings.twitch.startTimestamp)
-            .setEndTimestamp(config.settings.twitch.endTimestamp);
-
-          // Set the presence
-          client.user.setPresence(presence.toDiscord());
-        });
-
-      // Set the status
-      if (config.status === 'online' || config.status === 'idle' || config.status === 'dnd') {
-        client.user.setStatus(config.status);
-      }
-
-      if (config.status === 'offline' || config.status === 'invisible') {
-        console.log('Status cant be set to' + config.status + '\nPlease change the status in the config.json file');
-      }
-
-      // Done !
-      console.log(chalk.hex('#800080')('twitch RPC enabled successfully!'));
-      console.log(chalk.hex('#800080')('twitch: ' + config.settings.twitch.name));
-      console.log(chalk.hex('#800080')('Status: ' + config.status));
-
-    } catch (err) {
-      console.log(err);
-    }
-  });
+  } catch (err) {
+    console.error(err);
+  }
 }
