cb8811562004388d37e4cd107a4a32014adb341a
slow <slowdev14@gmail.com>
2021-12-20 16:12:20 +0100

    ok updated to resolve bugs but discord-selfbot rpc probably does not work anymore (sorry)

--- presences/twitch.js
+++ presences/twitch.js
@@ -1,33 +1,36 @@
-const chalk = require('chalk');
+const config = require('../config.json');
+const client = require('..');
+
 const rpc = require('discordrpcgenerator');
-
-const config = require('.././config.json');
-
-const uuid = () => ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, a => (a ^ Math.random() * 16 >> a / 4).toString(16));
+const chalk = require('chalk');
 
 if (config.mode === 'twitch') {
-
-  // Create the rich presence
-  const client = require('../index');
   client.on('ready', () => {
 
     try {
-
       rpc.getRpcImage(config.settings.twitch.applicationID, config.settings.twitch.largeImageKey)
         .then(image => {
           const presence = new rpc.Rpc()
-            .setUrl(config.settings.twitch.link)
+            .setName(config.settings.twitch.name)
+            .setUrl(config.settings.twitch.url)
+            .setDetails(config.settings.twitch.name)
             .setType('STREAMING')
+            .setState(config.settings.twitch.details)
             .setApplicationId(config.settings.twitch.applicationID)
-            .setAssetsLargeImage(image.id)
-            .setDetails(config.settings.twitch.name ? config.settings.twitch.name : undefined)
-            .setState(config.settings.twitch.state ? config.settings.twitch.state : undefined)
-            .setStartTimestamp(config.settings.spotify.startTimestamp ? config.settings.spotify.startTimestamp : undefined)
-            .setEndTimestamp(config.settings.spotify.endTimestamp ? config.settings.spotiify.endTimestamp : undefined)
+
+            .setAssetsLargeImage(config.settings.twitch.largeImageKey ? config.settings.twitch.largeImageKey : image.id)
+            .setAssetsLargeText(config.settings.twitch.largeImageText)
+
+            .setAssetsSmallImage(config.settings.twitch.smallImagekey ? config.settings.twitch.smallImageKey : undefined)
+            .setAssetsSmallText(config.settings.twitch.smallImageText ? config.settings.twitch.smallImageText : undefined)
+
+            .setStartTimestamp(config.settings.twitch.startTimestamp)
+            .setEndTimestamp(config.settings.twitch.endTimestamp);
 
           // Set the presence
-          client.user.setPresence(presence.toDiscord());;
-        })
+          client.user.setPresence(presence.toDiscord());
+        });
+
       // Set the status
       if (config.status === 'online' || config.status === 'idle' || config.status === 'dnd') {
         client.user.setStatus(config.status);
@@ -35,17 +38,15 @@ if (config.mode === 'twitch') {
 
       if (config.status === 'offline' || config.status === 'invisible') {
         console.log('Status cant be set to' + config.status + '\nPlease change the status in the config.json file');
-        process.exit(1);
       }
 
-      // Done!
-      console.log(chalk.hex('#800080')('Twitch RPC enabled successfully!'));
-      console.log(chalk.hex('#800080')('Twitch: ' + config.settings.twitch.name));
+      // Done !
+      console.log(chalk.hex('#800080')('twitch RPC enabled successfully!'));
+      console.log(chalk.hex('#800080')('twitch: ' + config.settings.twitch.name));
       console.log(chalk.hex('#800080')('Status: ' + config.status));
 
     } catch (err) {
       console.log(err);
-      console.log(chalk.hex('#800080')('Twitch RPC failed to enable!'));
     }
   });
 }
